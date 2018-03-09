321be58ca6594f357061e9b8c9cfc8983adece67
Alisa Ono <alisao@mit.edu>
2018-03-09 06:49:54 -0500

    Update map, network, bar examples; Update library versions

--- docs/components/examples/GroupedBarExample.jsx
+++ docs/components/examples/GroupedBarExample.jsx
@@ -3,21 +3,28 @@ import BarChart from "replot-bar"
 import {ComponentContainer, ColorTheme} from "replot-helpers"
 
 
-class BarExample extends React.Component {
+class GroupedBarExample extends React.Component {
 
   constructor(props){
     super(props)
     this.state = {
       optionList: [
-        {optionName: "data", optionType: "data", keyList: ["country"], weightKey: "population", initialValue: [
-          {population: 1373, country: "China"},
-          {population: 1266, country: "India"},
-          {population: 323, country: "United States"},
-          {population: 958, country: "Indonesia"},
-          {population: 805, country: "Brazil"},
-          {population: 501, country: "Pakistan"},
-          {population: 786, country: "Nigeria"},
-          {population: 456, country: "Bangladesh"},
+        {optionName: "data", optionType: "data", keyList: ["country", "year"], weightKey: "population", initialValue: [
+          {country: "Github", year: 2015, population: 11*100000},
+          {country: "Github", year: 2016, population: 16*100000},
+          {country: "Github", year: 2017, population: 24*100000},
+          {country: "Netherlands", year: 2015, population: 16.925*100000},
+          {country: "Netherlands", year: 2016, population: 16.987*100000},
+          {country: "Netherlands", year: 2017, population: 17.035*100000},
+          {country: "Israel", year: 2015, population: 8.064*100000},
+          {country: "Israel", year: 2016, population: 8.191*100000},
+          {country: "Israel", year: 2017, population: 8.321*100000},
+          {country: "New Zealand", year: 2015, population: 4.529*100000},
+          {country: "New Zealand", year: 2016, population: 4.66*100000},
+          {country: "New Zealand", year: 2017, population: 4.705*100000},
+          {country: "Iceland", year: 2015, population: 0.329*100000},
+          {country: "Iceland", year: 2016, population: 0.332*100000},
+          {country: "Iceland", year: 2017, population: 0.335*100000},
         ]},
         {optionName: "width", name: "Width", optionType: "field", input: "string", initialValue: "98%"},
         {optionName: "height", name: "Height", optionType: "field", input: "number", initialValue: 450},
@@ -31,8 +38,12 @@ class BarExample extends React.Component {
         {optionName: "graphTitleColor", name: "Title Color", optionType: "field", initialValue: ColorTheme[this.props.palette].axisColor},
         {optionName: "labelColor", name: "Label Color", optionType: "field", initialValue: ColorTheme[this.props.palette].axisColor},
         {optionName: "gridColor", name: "Grid Color", optionType: "field", initialValue: ColorTheme[this.props.palette].axisColor},
+        {optionName: "legendFontColor", name: "Legend Color", optionType: "field", input: "string", initialValue: ColorTheme[this.props.palette].axisColor},
+        {optionName: "legendShowBorder", name: "Show Legend Border", optionType: "bool", initialValue: false},
+        {optionName: "legendBorderColor", name: "Legend Border Color", optionType: "field", input: "string", initialValue: ColorTheme[this.props.palette].axisColor},
         {optionName: "xKey", name: "X Axis", optionType: "hidden", initialValue: "country"},
         {optionName: "yKey", name: "Y Axis", optionType: "hidden", initialValue: "population"},
+        {optionName: "groupKey", name: "Group", optionType: "hidden", initialValue: "year"}
       ],
       barScale: "lin",
     }
@@ -52,7 +63,7 @@ class BarExample extends React.Component {
 
     return(
       <div className="container" style={style.container}>
-        <h1 style={style.title}> Bar Chart </h1>
+        <h1 style={style.title}> Grouped Bar Chart </h1>
         <ComponentContainer optionList={this.state.optionList}
           palette={this.props.palette}>
           <BarChart data={this.state.optionList[0].initialValue}
@@ -65,4 +76,4 @@ class BarExample extends React.Component {
 }
 
 
-export default BarExample
+export default GroupedBarExample
--- docs/sections/ExamplesSection.jsx
+++ docs/sections/ExamplesSection.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import {SectionContainer} from "replot-helpers"
 import TreeExample from "../components/examples/TreeExample.jsx"
 import BarExample from "../components/examples/BarExample.jsx"
-import GroupedExample from "../components/examples/GroupedExample.jsx"
+import GroupedBarExample from "../components/examples/GroupedBarExample.jsx"
 import LineExample from "../components/examples/LineExample.jsx"
 import ScatterExample from "../components/examples/ScatterExample.jsx"
 import MapExample from "../components/examples/MapExample.jsx"
@@ -17,8 +17,8 @@ class ExamplesSection extends React.Component {
       <SectionContainer>
         <TreeExample palette={this.props.palette}/>
         <BarExample palette={this.props.palette}/>
+        <GroupedBarExample palette={this.props.palette}/>
         <BoxPlotExample palette={this.props.palette}/>
-        <GroupedExample palette={this.props.palette}/>
         <LineExample palette={this.props.palette}/>
         <ScatterExample palette={this.props.palette}/>
         <MapExample palette={this.props.palette} />
